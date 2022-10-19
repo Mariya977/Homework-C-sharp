48e0b10b0b8bb76183cf80a47bc5f37df12df4ac
Mariya977 <snezok971@yandex.ru>
2022-10-19 14:04:00 +0300

    First task solved

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string stringnumbers = Convert.ToString(numbers);
+// int length = stringnumbers.Length;
+Console.WriteLine("вторая цифра этого числа -> "+stringnumbers[1]);
+// if (stringnumbers.Length > 100 && stringnumbers.Length < 999 )
+// {
+//     Console.WriteLine("вторая цифра этого числа -> "+stringnumbers[1]);
+// }
+// else
+// {
+//     Console.WriteLine("Вы ввели не трехначное число");
+// }
+
+// Не получается ограничить программу трехзначным числом, подскажите, как можно это сделать, мой способ закомментированный почему-то не работает...
+
+
