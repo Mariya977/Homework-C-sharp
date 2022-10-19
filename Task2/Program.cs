d890fb81c28c2b59e348d6b589674f7aa8623853
Mariya977 <snezok971@yandex.ru>
2022-10-19 14:43:42 +0300

    Secons task solved

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int AnyNumber = Convert.ToInt32(Console.ReadLine());
+string AnyNumberString = Convert.ToString(AnyNumber);
+if (AnyNumber > 99)
+{
+    Console.WriteLine("третья цифра этого числа -> "+AnyNumberString[2]);
+}
+else
+{
+    Console.WriteLine("В данном числе нет третьей цифры");
+}
